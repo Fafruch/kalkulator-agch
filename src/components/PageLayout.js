748d8240d12b8f470cb2e68c5232ead89f066e8f
Fafruch <fafruszek@gmail.com>
2017-07-20 02:24:38 +0200

    SubjectsContainter renamed to Subjects and moved to 'components' catalog

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Introduction from '../components/Introduction'
-import SubjectsContainer from '../containers/SubjectsContainer'
+import Subjects from './Subjects'
 import CalculatorContainer from '../containers/CalculatorContainer'
 import Footer from '../components/Footer'
 import { primarySubjects, lingualSubjects } from '../constants/SubjectTypes'
@@ -11,9 +11,9 @@ const PageLayout = () => (
     <div className='container'>
       <Header />
       <Introduction />
-      <SubjectsContainer subjectsType={primarySubjects} />
-      <SubjectsContainer subjectsType={lingualSubjects} />
-        <CalculatorContainer />
+      <Subjects subjectsType={primarySubjects} />
+      <Subjects subjectsType={lingualSubjects} />
+      <CalculatorContainer />
     </div>
     <div>
       <Footer />
--- src/components/Subjects.js
+++ src/components/Subjects.js
@@ -4,15 +4,15 @@ import SubjectListContainer from '../containers/SubjectListContainer'
 import AddSubjectContainer from '../containers/AddSubjectContainer'
 import { primarySubjects } from '../constants/SubjectTypes'
 
-const SubjectsContainer = ({ subjectsType }) => (
+const Subjects = ({ subjectsType }) => (
   <div className='mt-5 mb-5'>
     <h2>{subjectsType === primarySubjects ? 'Przedmioty główne' : 'Języki'} </h2>
     <SubjectListContainer subjectsType={subjectsType} />
     <AddSubjectContainer subjectsType={subjectsType} />
   </div>
 )
-SubjectsContainer.propTypes = {
+Subjects.propTypes = {
   subjectsType: PropTypes.string.isRequired
 }
 
-export default SubjectsContainer
+export default Subjects
