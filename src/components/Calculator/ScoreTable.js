57661a3c1ab2f7fd57cc96fec3d8b71e072d65b8
Fafruch <fafruszek@gmail.com>
2017-08-16 00:26:48 +0200

    Thresholds JSON added, basic CSS for thresholds which display on the right of scores in table. TO DO: button to show/close thresholds, "Informatyka i Ekonometria" in courses.json changed to "Informatyka"

--- src/components/Calculator/ScoreTable.js
+++ src/components/Calculator/ScoreTable.js
@@ -2,62 +2,127 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import getCourseScore from '../../utils/getCourseScore'
-import facultiesArray from '../../faculties.json'
 
-const ScoreTable = ({ subjects, onToggleClick }) => (
-  <div>
-    <br />
-    <br />
-    <table className='table table-bordered score-table'>
-      <tbody>
-        <tr>
-          <th scope='row'>
-            Wydział
-          </th>
-          <th scope='row'>
-            Kierunek
-          </th>
-          {/* <th scope='row'>
-            Przedmioty
-          </th> */}
-          <th scope='row'>
-            Wynik
-          </th>
-        </tr>
-        {facultiesArray.map((faculty) =>
-          faculty.courses.map((course, courseIndex) =>
-            <tr key={courseIndex}>
-              {!courseIndex &&
-              <td rowSpan={faculty.courses.length}>
+import coursesArray from '../../courses.json'
+import thresholdsArray from '../../thresholds.json'
+
+const ScoreTable = ({ subjects }) => {
+  let coursesArrayScores = []
+
+  coursesArray.forEach((course) => {
+    coursesArrayScores.push(getCourseScore(course, subjects))
+  })
+
+  return (
+    <div>
+      <br />
+      <br />
+      <table className='table table-bordered score-table'>
+        <tbody>
+          <tr>
+            <th />
+            <th />
+            <th />
+            <th colSpan='4' className='thresholds-title'>
+              Progi z zeszłych lat
+            </th>
+          </tr>
+          <tr>
+            <th scope='row'>
+              Wydział
+            </th>
+            <th scope='row'>
+              Kierunek
+            </th>
+            {/* <th scope='row'>
+              Przedmioty
+            </th> */}
+            <th scope='row'>
+              Wynik
+            </th>
+            <th>
+              2013
+            </th>
+            <th>
+              2014
+            </th>
+            <th>
+              2015
+            </th>
+            <th>
+              2016
+            </th>
+          </tr>
+          {coursesArray.map((course, i, coursesArray2) =>
+            <tr key={i}>
+              <td>
                 <strong>
-                  {faculty.name}
+                  {(i !== 0 && coursesArray2[i - 1].faculty === course.faculty) ? null : course.faculty }
                 </strong>
-              </td>}
+              </td>
               <td>
                 {course.name}
               </td>
               {/* <td>
-              {course.subjects.join(', ')}
-            </td> */}
+                {course.subjects.join(', ')}
+              </td> */}
               <td>
                 <strong>
-                  {getCourseScore(course, subjects)}
+                  {coursesArrayScores[i]}
                 </strong>
               </td>
+              <td>
+                {coursesArrayScores[i] > thresholdsArray[i]['2013']
+                  ? <div className='score-under'>
+                    {thresholdsArray[i]['2013']}
+                  </div>
+                  : <div className='score-above'>
+                    {thresholdsArray[i]['2013']}
+                  </div>
+                }
+              </td>
+              <td>
+                {coursesArrayScores[i] > thresholdsArray[i]['2014']
+                  ? <div className='score-under'>
+                    {thresholdsArray[i]['2014']}
+                  </div>
+                  : <div className='score-above'>
+                    {thresholdsArray[i]['2014']}
+                  </div>
+                }
+              </td>
+              <td>
+                {coursesArrayScores[i] > thresholdsArray[i]['2015']
+                  ? <div className='score-under'>
+                    {thresholdsArray[i]['2015']}
+                  </div>
+                  : <div className='score-above'>
+                    {thresholdsArray[i]['2015']}
+                  </div>
+                }
+              </td>
+              <td>
+                {coursesArrayScores[i] > thresholdsArray[i]['2016']
+                  ? <div className='score-under'>
+                    {thresholdsArray[i]['2016']}
+                  </div>
+                  : <div className='score-above'>
+                    {thresholdsArray[i]['2016']}
+                  </div>
+                }
+              </td>
             </tr>
-          )
-        )
-        }
-      </tbody>
-    </table>
-  </div>
-)
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 ScoreTable.propTypes = {
   subjects: PropTypes.shape({
     primary: PropTypes.array.isRequired,
     lingual: PropTypes.array.isRequired
-  }).isRequired,
-  onToggleClick: PropTypes.func.isRequired
+  }).isRequired
 }
 
 export default ScoreTable
